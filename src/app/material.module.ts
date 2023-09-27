a04ef9f92f51474c5954716f20db535b1f3f30bd
juanpabloletteri <pabloletteri@gmail.com>
2023-09-27 15:39:50 -0300

    traer datos + estilos

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-exports: [MatToolbarModule, MatIconModule]
+    exports: [MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule]
 })
 
-export class MaterialModule{}
+export class MaterialModule { }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './services/products.service';
+import { tap } from 'rxjs/operators';
+import { Product } from './interfaces/product.interface';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  products!: Product[];
+
+  constructor(private productsSvc: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productsSvc.getProducts()
+      .pipe(
+        tap((products: Product[]) => this.products = products)
+        )
+      .subscribe()
+  }
+
 
 }
